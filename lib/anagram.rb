527c709c244f5a815077e03e505c70ea8967db42
Scott Weinstein <sweinstein27@gmail.com>
2018-05-11 14:42:59 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,16 +5,12 @@ class Anagram
     @word = word
   end
 
-  def match(anagram) 
-  anagram.find_all do |words| 
-      if words.split("").sort == self.word.split("").sort 
+  def match(anagram)
+  anagram.find_all do |words|
+      if words.split("").sort == self.word.split("").sort
         word s
       end
-    end 
+    end
   end
 
 end
-
-
-
-  
